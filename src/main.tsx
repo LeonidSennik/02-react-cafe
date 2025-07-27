16c22d7b78966f22a03adbd08a890ac1d9432669
LeonidSennik <barodfsh@gmail.com>
2025-07-27 15:46:06 +0300

    up

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import './style.css'
+import 'modern-normalize/modern-normalize.css'; 
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './components/App/App';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
